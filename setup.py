f870b6a204511ee680e27129bf569ce12c00b0d3
ellisrichardj <richard.ellis@apha.gov.uk>
2024-12-20 15:24:25 +0000

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='srst2',
-    version='0.2.0',
+    version='0.3.0',
     author='Kathryn Holt',
     author_email='drkatholt@gmail.com',
     packages=['srst2'],
